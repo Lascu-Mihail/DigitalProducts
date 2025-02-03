77d116858340f44748a2985e74eea32280ac5797
Mihai <lascumihai31@gmail.com>
2025-02-03 13:49:41 +0200

    Creating Pom xml

---
+++ src/main/java/com/digitalstore/EssenceVerse/EssenceVerseApplication.java
@@ -0,0 +1,13 @@
+package com.digitalstore.EssenceVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssenceVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssenceVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalstore/EssenceVerse/EssenceVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalstore.EssenceVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssenceVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
